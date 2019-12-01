b8313606356c1a9e5d4bb201078fdefc7a8f0ac1
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2019-12-02 00:54:38 +0900

    Add solution for day 1 part 2

--- day-1/day1.rb
+++ day-1/day1.rb
@@ -2,6 +2,11 @@ class Day1
   def self.calc_fuel(mass)
     (mass / 3).floor - 2
   end
+
+  def self.calc_fuel_part2(mass)
+    return 0 unless self.calc_fuel(mass).positive?
+    self.calc_fuel(mass) + self.calc_fuel_part2(self.calc_fuel(mass))
+  end
 end
 
 p File
@@ -9,4 +14,11 @@ p File
   .lines
   .map(&:to_i)
   .map {|mass| Day1.calc_fuel(mass)}
+  .sum
+
+p File
+  .read('input')
+  .lines
+  .map(&:to_i)
+  .map {|mass| Day1.calc_fuel_part2(mass)}
   .sum
