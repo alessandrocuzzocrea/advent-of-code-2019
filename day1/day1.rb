45bfa94083401dbd130df1c5f4132112d5cc78e8
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2019-12-02 01:04:19 +0900

    Refactor (rubocop -a)

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class Day1
   def self.calc_fuel(mass)
     (mass / 3).floor - 2
   end
 
   def self.calc_fuel_part2(mass)
-    return 0 unless self.calc_fuel(mass).positive?
-    self.calc_fuel(mass) + self.calc_fuel_part2(self.calc_fuel(mass))
+    return 0 unless calc_fuel(mass).positive?
+
+    calc_fuel(mass) + calc_fuel_part2(calc_fuel(mass))
   end
 end
 
@@ -13,12 +16,12 @@ p File
   .read('input')
   .lines
   .map(&:to_i)
-  .map {|mass| Day1.calc_fuel(mass)}
+  .map { |mass| Day1.calc_fuel(mass) }
   .sum
 
 p File
   .read('input')
   .lines
   .map(&:to_i)
-  .map {|mass| Day1.calc_fuel_part2(mass)}
+  .map { |mass| Day1.calc_fuel_part2(mass) }
   .sum
--- day1/tests.rb
+++ day1/tests.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require './day1'
 require 'byebug'
 
 class Day1Test < Minitest::Test
   def test_day1_part1
-    assert_equal 654,   Day1.calc_fuel(1969)
-    assert_equal 33583, Day1.calc_fuel(100756)
+    assert_equal 654, Day1.calc_fuel(1969)
+    assert_equal 33_583, Day1.calc_fuel(100_756)
   end
 
   def test_day1_part2
     assert_equal 2,     Day1.calc_fuel_part2(14)
     assert_equal 966,   Day1.calc_fuel_part2(1969)
-    assert_equal 50346, Day1.calc_fuel_part2(100756)
+    assert_equal 50_346, Day1.calc_fuel_part2(100_756)
   end
 end
