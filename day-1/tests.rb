4787b7890b6b6fa16b61b0b7e2d4b077790ccfa2
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2019-12-01 23:36:09 +0900

    Rename day1 test method name

--- day-1/tests.rb
+++ day-1/tests.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require './day1'
 
 class Day1Test < Minitest::Test
-  def test_loller
+  def test_day1_part1
     assert_equal 654,   Day1.calc_fuel(1969)
     assert_equal 33583, Day1.calc_fuel(100756)
   end
