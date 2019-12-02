c54b747a3ea8c483a5122f4bf9d451df0cd19e52
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2019-12-02 20:25:00 +0900

    Add (quick and super dirty) solution to day 2-2

--- day2/day2.rb
+++ day2/day2.rb
@@ -10,3 +10,18 @@ computer.poke(2, 2)
 computer.run
 
 p computer
+
+# part 2
+99.times do |noun|
+  99.times do |verb|
+    computer = ShipComputer.new
+    computer.load(File.read('input'))
+    computer.poke(1, noun)
+    computer.poke(2, verb)
+    computer.run
+    if computer.peek(0) == 19_690_720
+      p "noun #{noun}, verb #{verb}"
+      p "res #{100 * noun + verb}"
+    end
+  end
+end
